b300795389459fd0168f2f6ed14aa173642284cf
Carla Galarza <Carla.M.Galarza.13@dartmouth.edu>
2015-03-11 15:49:01 -0400

    Removed FFmpeg binaries, instead the resposibility of installing ffmpeg is left upto the user. If ffmpeg isn't installed an error message is displayed. If a user doesn't want to use ffmpeg the tool can simply be removed from the FITS configuration file.

--- src/edu/harvard/hul/ois/fits/tools/ffmpeg/FFmpeg.java
+++ src/edu/harvard/hul/ois/fits/tools/ffmpeg/FFmpeg.java
@@ -1,31 +1,29 @@
-/* 
- * Copyright 2015 Trustees of Dartmouth College
- * 
+/*
  * This file is part of FITS (File Information Tool Set).
  *
- * This class adds FFmpeg as a tool to FITS. The main purpose of 
- * adding FFmpeg is to identify and validate video files, thought tools 
- * like ffmpeg and ffprobe could be used to provide other valuable 
- * information about the file itself (ex: frame rate, codec). 
+ * This class adds FFmpeg as a tool to FITS. The main purpose of
+ * adding FFmpeg is to identify and validate video files, though tools
+ * like ffmpeg and ffprobe could be used to provide other valuable
+ * information about the file itself (ex: frame rate, codec).
  *
- * FFmpeg binary executables were added to the FITS for both 32-bit and 
- * 64-bit Linux and Mac OS X. Windows executables are not added, but 
- * could be at a later point. Currently the version number of FFmpeg in use is 
- * 2.5.3
+ * FFmpeg and FFprobe (usually comes with FFmpeg) needs to be installed on
+ * the computer running ffmpeg. Including binaries would increase the size of
+ * FITS because many system specific binaries would need to be included. This
+ * leaves the resposibility of updating FFmpeg to the user.
  *
- * TODO: Add something about ffmpeg licensing.
+ * Copyright 2015 Trustees of Dartmouth College
  *
- * 
+ *
  * FITS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * FITS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with FITS.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -53,29 +51,29 @@ import edu.harvard.hul.ois.fits.tools.ToolInfo;
 import edu.harvard.hul.ois.fits.tools.ToolOutput;
 import edu.harvard.hul.ois.fits.tools.utils.CommandLine;
 
-/** 
+/**
  *  The glue class for invoking FFmpeg under FITS.
  *
- * 	The purpose adding FFmpeg to FITS is to identify and validate video files. This class 
- * 	generates FITS XML and FFprobe XML. The FFprobe XML is transformed to use the
- * 	FITS schema.
+ * 	The purpose adding FFmpeg to FITS is to identify and validate video files.
+ * 	This class generates FITS XML and FFprobe XML. The FFprobe XML is
+ * 	transformed to use the FITS schema.
  *
- * 	FFmpeg, as used in this class is limited to identifiying and validating video files 
- * 	ONLY. Thought it could be used to validate other formats (image and audio), in 
- * 	the future. 
+ * 	FFmpeg, as used in this class is limited to identifiying and validating video
+ * 	files ONLY. Though it could be used to validate other formats (image and
+ * 	audio), in the future.
  *
- * 	There is logic for running this tool in Linux and Mac OS X machines, not Windows.
- * 
+ * 	The resposibility of installing FFmpeg is left up to the user. This class
+ * 	checks if ffmpeg is available and if not it does not use the tool. This class
+ * 	has not been tested on a windows machine.
+ *
  */
 public class FFmpeg extends ToolBase {
 
 	public final static String XSLT = Fits.FITS_XML + "ffmpeg/ffmpeg_to_fits.xslt";
+	public final static String FFPROBE = "ffprobe";
+	public final static String FFMPEG	= "ffmpeg";
 
-	private final static String MAC_64BIT	= Fits.FITS_TOOLS + "ffmpeg/Mac_OS_X/64bit/";
-	private final static String MAC_32BIT	= Fits.FITS_TOOLS + "ffmpeg/Mac_OS_X/32bit/";
-	private final static String LINUX_64BIT	= Fits.FITS_TOOLS + "ffmpeg/Linux/64bit/";
-	private final static String LINUX_32BIT	= Fits.FITS_TOOLS + "ffmpeg/Linux/32bit/";
-	private String ffmpegBuild;
+	private boolean installed = false;
 
 	private final static String TOOL_NAME = "FFmpeg";
 	private boolean enabled = true;
@@ -86,83 +84,67 @@ public class FFmpeg extends ToolBase {
         logger.debug ("Initializing ffmpeg");
 
 		String osName = System.getProperty("os.name");
-		String osArch = System.getProperty("os.arch");
 
 		info = new ToolInfo();
 		info.setName(TOOL_NAME);
 
-		// Figure out which ffmpeg executable to run.
-		if (osName.startsWith("Mac OS X")) {
-			if (osArch.contains("64") || osArch.contains("86")) {
-				ffmpegBuild = MAC_64BIT;
-				info.setNote("FFmpeg for Mac OS X 64 bit");
-				logger.debug("FFmpeg will use Mac OS X 64 bit executable.");
-			} else {
-				ffmpegBuild = MAC_32BIT;
-				info.setNote("FFmpeg for Mac OS X 32 bit");
-				logger.debug("FFmpeg will use Mac OS X 32 bit executable.");
-			}
-		} else if (osName.startsWith("Linux")){
-			if (osArch.contains("64") || osArch.contains("86")){
-				ffmpegBuild = LINUX_64BIT;
-				info.setNote("FFmpeg for Linux 64 bit");
-            	logger.debug("FFmpeg will use Linux 64 bit executable.");
-            } else {
-            	ffmpegBuild = LINUX_32BIT;
-				info.setNote("FFmpeg for Linux 32 bit");
-            	logger.debug("FFmpeg will use Linux 32 bit executable.");
-            }
+		// Check if the system has ffmpeg installed.
+		if ((osName.startsWith("Mac OS X") || osName.startsWith("Linux")) &&
+			isInstalled(FFMPEG) && isInstalled(FFPROBE)){
+				installed = true;
 		} else {
-		    logger.error ("Could not find executables for " + osName + " " + osArch + "not running ffmpeg.");
-			throw new FitsToolException("FFmpeg cannot be used on this system");
+			setEnabled(false);
+		    logger.error("FFmpeg not installed. Not running FFmpeg.");
+			throw new FitsToolException("FFmpeg cannot be found on this system." +
+				"Please install ffmpeg or remove tool from FITS configuration file.");
 		}
 
 		// Extract version number. Each tool needs to set the version number in the constructor.
 		List<String> versionCommand = new ArrayList<String>(
-			Arrays.asList (ffmpegBuild + "ffprobe", "-hide_banner", "-show_entries", 
+			Arrays.asList (FFPROBE, "-hide_banner", "-show_entries",
 				"program_version=version", "-print_format", "default=nw=1:nk=1"));
 		info.setVersion(CommandLine.exec(versionCommand, null).trim());
 
 	}
 
-	// Called by FITS to invoke the tool against the input file. It must return a ToolOutput object
-	// containing valid FITS XML.
+	// Called by FITS to invoke the tool against the input file. It must return
+	// a ToolOutput object containing valid FITS XML.
 	public ToolOutput extractInfo(File file) throws FitsToolException {
         logger.debug("Exiftool.extractInfo starting on " + file.getName());
 		long startTime = System.currentTimeMillis();
 
 		// Validating file.
 		List<String> valCommand = new ArrayList<String>(
-			Arrays.asList(ffmpegBuild + "ffmpeg", "-v", "error", 
+			Arrays.asList(FFMPEG, "-v", "error",
 				"-i", file.getPath(), "-f", "null", "-"));
 
 		// Extracting identification information.
 		List<String> idenCommand = new ArrayList<String>(
-			Arrays.asList(ffmpegBuild + "ffprobe", "-v", "quiet", "-show_format", 
+			Arrays.asList(FFPROBE, "-v", "quiet", "-show_format",
 				"-show_error", "-show_program_version", "-print_format", "xml", file.getPath()));
-		
+
 		logger.debug("Launching ffmpeg, command = " + valCommand);
 		String valOut = CommandLine.exec(valCommand, null);
-		
+
 		logger.debug("Launching ffmpeg, command = " + idenCommand);
 		String idenOut = CommandLine.exec(idenCommand, null);
-		
+
 		logger.debug("Finished running FFmpeg");
-		
+
 		Document ffprobeXML = createXml(idenOut, valOut);
 
 		Document fitsXML = transform(XSLT, ffprobeXML);
 
 		output = new ToolOutput(this, fitsXML, ffprobeXML);
-		
+
 		duration = System.currentTimeMillis() - startTime;
 		runStatus = RunStatus.SUCCESSFUL;
         logger.debug("FFmpeg.extractInfo finished on " + file.getName());
 		return output;
 	}
-	
-	private Document createXml(String ffprobeOut, String ffmpegOut) throws FitsToolException {    	
-    	
+
+	private Document createXml(String ffprobeOut, String ffmpegOut) throws FitsToolException {
+
     	Document ffprobeXML = null;
 		try {
 			ffprobeXML = saxBuilder.build(new StringReader(ffprobeOut));
@@ -171,7 +153,7 @@ public class FFmpeg extends ToolBase {
 			throw new FitsToolException("Error parsing ffprobe XML Output", e);
 		}
 
-		if (!ffmpegOut.isEmpty()){ // or check if its null?
+		if (!ffmpegOut.isEmpty()){
 			Element error = ffprobeXML.getRootElement().getChild("error");
 			if (error == null){
 				error = new Element("error");
@@ -179,16 +161,23 @@ public class FFmpeg extends ToolBase {
 				ffprobeXML.getRootElement().addContent(error);
 			}
 		}
-		
+
         return ffprobeXML;
     }
+
+	// Checks whether or not the command passed in exists.
+	public boolean isInstalled(String command) throws FitsToolCLIException{
+		ArrayList<String> cmd = new ArrayList<String>(Arrays.asList("which", command));
+		String output = CommandLine.exec(cmd, null);
+		return !(output == null || output.isEmpty());
+	}
 
 	public boolean isEnabled() {
 		return enabled;
 	}
 
 	public void setEnabled(boolean value) {
-		enabled = value;		
+		enabled = value;
 	}
-	
+
 }
